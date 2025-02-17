cd18a27fe742f55bc1a90579b73a1074a541b529
BryanHatley <bryan.hatley2019@gmail.com>
2025-02-16 20:29:09 -0500

    Baseline

---
+++ Project-One-Backend/src/main/java/com/revature/Project1Backend/ProjectOneBackendApplication.java
@@ -0,0 +1,13 @@
+package com.revature.Project1Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOneBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectOneBackendApplication.class, args);
+	}
+
+}
